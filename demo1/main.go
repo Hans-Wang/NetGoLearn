4e67e3d42d0732f1b5b6c79c5317f37e37864460
Hans <hanshao925@126.com>
2020-03-16 12:03:22 +0800

    first gin web

---
+++ demo1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func testHandle(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"message": "test",
+	})
+}
+func main(){
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/test", testHandle)
+	r.Run()
+}
